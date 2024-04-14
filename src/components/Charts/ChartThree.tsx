1e6b76192511b7d88286d0acd5b0f0b7ccf2f017
Pratyush <pratyush.ee22@bmsce.ac.in>
2024-04-14 21:27:46 +0530

    Crime Analytics-1

--- src/components/Charts/ChartThree.tsx
+++ src/components/Charts/ChartThree.tsx
@@ -123,7 +123,7 @@ const ChartThree: React.FC = () => {
           <div className="flex w-full items-center">
             <span className="mr-2 block h-3 w-full max-w-3 rounded-full bg-primary"></span>
             <p className="flex w-full justify-between text-sm font-medium text-black dark:text-white">
-              <span> Desktop </span>
+              <span> Robbery </span>
               <span> 65% </span>
             </p>
           </div>
@@ -132,7 +132,7 @@ const ChartThree: React.FC = () => {
           <div className="flex w-full items-center">
             <span className="mr-2 block h-3 w-full max-w-3 rounded-full bg-[#6577F3]"></span>
             <p className="flex w-full justify-between text-sm font-medium text-black dark:text-white">
-              <span> Tablet </span>
+              <span> Eve Teasing </span>
               <span> 34% </span>
             </p>
           </div>
@@ -141,7 +141,7 @@ const ChartThree: React.FC = () => {
           <div className="flex w-full items-center">
             <span className="mr-2 block h-3 w-full max-w-3 rounded-full bg-[#8FD0EF]"></span>
             <p className="flex w-full justify-between text-sm font-medium text-black dark:text-white">
-              <span> Mobile </span>
+              <span> Accident </span>
               <span> 45% </span>
             </p>
           </div>
@@ -150,7 +150,7 @@ const ChartThree: React.FC = () => {
           <div className="flex w-full items-center">
             <span className="mr-2 block h-3 w-full max-w-3 rounded-full bg-[#0FADCF]"></span>
             <p className="flex w-full justify-between text-sm font-medium text-black dark:text-white">
-              <span> Unknown </span>
+              <span> Others </span>
               <span> 12% </span>
             </p>
           </div>
